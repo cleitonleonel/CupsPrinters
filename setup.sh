e43138d773a2bd0272bdd1a69bb1b662c2711116
cleitonLC <cleiton.leonel@gmail.com>
2021-03-11 11:31:30 -0300

    pequena correção no setup...

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ echo 'Instalando libs extra...'
 sudo apt install libhdf5-dev -y
 sudo apt install libpq-dev -y
 sudo apt install libssl-dev zlib1g-dev gcc g++ make -y
-sudo apt-get install wkhtmltopdf -y
+sudo apt install wkhtmltopdf -y
 sudo apt install cups -y
 sudo apt install build-essential cmake libcups2-dev libcupsimage2-dev system-config-printer -y
 sudo apt install cups-bsd -y
