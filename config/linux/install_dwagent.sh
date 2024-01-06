141dcae1849e43f59029b25ae21d787c88469bc0
cleiton <cleiton.leonel@gmail.com>
2024-01-06 16:28:20 -0300

    Inclusão de um utilitário para conversão de imagens.

--- config/linux/install_dwagent.sh
+++ config/linux/install_dwagent.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Define o diretório onde os arquivos do Dwagent serão instalados
-DIR="$HOME/Downloads"
+DIR="/usr/share/dwagent/runtime/bin/"
 
 # Função para instalação do Dwagent
 install() {
   # Baixa o script dwagent.sh
-  wget https://www.dwservice.net/download/dwagent.sh
-  # Dá permissões de execução ao script
-  sudo chmod +x ./dwagent.sh
-  # Executa o script dwagent.sh
-  sudo ./dwagent.sh
+  if wget https://www.dwservice.net/download/dwagent.sh; then
+    # Dá permissões de execução ao script
+    chmod +x ./dwagent.sh
+    # Executa o script dwagent.sh
+    sudo ./dwagent.sh
+  else
+    echo "Failed to download dwagent.sh. Exiting."
+    exit 1
+  fi
 }
 
 # Verifica se o diretório não existe
@@ -24,28 +28,28 @@ sudo systemctl stop dwagent.service
 sudo systemctl disable dwagent.service
 
 # Diretório de configuração para o usuário
-cd "$HOME" || exit
-# Cria o diretório se ele não existir
-sudo mkdir -p .config/systemd/user
-cd .config/systemd/user/ || exit
+su - "$USER" -c "mkdir -p $HOME/.config/systemd/user"
 
 # Move o arquivo de serviço para o diretório de configuração do usuário
-sudo mv /etc/systemd/system/dwagent.service .
+sudo mv /etc/systemd/system/dwagent.service "$HOME/.config/systemd/user/"
 # Altera as permissões do arquivo para o usuário atual
-sudo chown "$USER:$USER" dwagent.service
+sudo chown "$USER:$USER" "$HOME/.config/systemd/user/dwagent.service"
 
 # Modifica o arquivo de serviço para usar default.target
-sed -i 's/WantedBy=multi-user.target/WantedBy=default.target/' ~/.config/systemd/user/dwagent.service
+sed -i "s/WantedBy=multi-user.target/WantedBy=default.target/" "$HOME/.config/systemd/user/dwagent.service"
 
 # Define as permissões do diretório do Dwagent
 sudo chown -R "$USER:$USER" /usr/share/dwagent
+
 # Habilita o login do usuário no sistema
 sudo loginctl enable-linger "$USER"
 
+echo "Intalação efetuada com sucesso, ativando serviços..."
+
 # Ativa e inicia o serviço dwagent.service para o usuário
-sudo systemctl --user enable dwagent.service
-sudo systemctl --user start dwagent.service
+systemctl --user enable dwagent.service
+systemctl --user start dwagent.service
 
 # Descomente as linhas abaixo para ativar o serviço usando XDG_RUNTIME_DIR
-XDG_RUNTIME_DIR=/run/user/$UID systemctl --user enable dwagent.service
-XDG_RUNTIME_DIR=/run/user/$UID systemctl --user start dwagent.service
+# XDG_RUNTIME_DIR=/run/user/$UID systemctl --user enable dwagent.service
+# XDG_RUNTIME_DIR=/run/user/$UID systemctl --user start dwagent.service
