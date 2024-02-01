bf2821317764692ee7ade991322160ead78aa8c0
cleiton <cleiton.leonel@gmail.com>
2024-02-01 17:17:56 -0300

    Correção do instalador dwagent e ajustes nos arquivos de configuração do putty no windows.

---
+++ check_process.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+log_file="mobile_log.txt"
+limit=1024  # 1 MB em kilobytes
+current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
+
+check_process() {
+  check_log
+  ps -C mobile > /dev/null
+}
+
+check_log(){
+  tamanho=$(du -k "$log_file" | cut -f1)
+
+  if [ "$tamanho" -gt "$limit" ]; then
+      echo -n > "$log_file"
+  fi
+}
+
+check_process
+
+if [ $? -ne 0 ]; then
+  mensagem="${current_datetime} [INFO] : O processo 'mobile' não está em execução."
+  echo "$mensagem" >> "$log_file"
+  mobile >> "$log_file" 2>&1 &
+else
+  mensagem="${current_datetime} [INFO] : O processo 'mobile' está em execução."
+  echo "$mensagem" >> "$log_file"
+fi
+
